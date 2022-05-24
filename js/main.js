0c872e78cf464fb4ca216eeed070cc961e4de46d
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-24 17:16:38 +0600

    refactor: code refactor

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-function validateForm(){
-    for(var i=0; i<myform.elements.length; i++){
-        if(myform.elements[i].className=="red" && myform.elements[i].value.length == 0){
-            alert('Please fill in all required fields');
-            return false;
-        }
+function validateForm() {
+  console.log();
+  for (var i = 0; i < myform.elements.length; i++) {
+    if (
+      myform.elements[i].className == "red" &&
+      myform.elements[i].value.length == 0
+    ) {
+      alert("Please fill in all required fields");
+      return false;
     }
+  }
 }
